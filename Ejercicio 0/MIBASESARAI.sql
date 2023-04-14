5fa81576e8f2731cb6ae1dc4de0515886b054ff4
Sarai Blanco <saraiblancosalgado2014@gmail.com>
2023-04-14 16:29:27 -0400

    Ejercicio 0

---
+++ Ejercicio 0/MIBASESARAI.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE MIBASESARAI;
+
+USE MIBASESARAI;
+
+CREATE TABLE INSCRIPCION (
+    CIestudiante int  NOT NULL,
+    sigla varchar(10)  NOT NULL,
+    nota1 int  NOT NULL,
+    nota2 int  NOT NULL,
+    nota3 int  NOT NULL,
+    notaFinal int  NOT NULL,
+    CONSTRAINT INSCRIPCION_pk PRIMARY KEY (CIestudiante)
+);
+
+CREATE TABLE PERSONA (
+    CI int  NOT NULL,
+    nombreCompleto varchar(45)  NOT NULL,
+    fechaDeNacimiento date  NOT NULL,
+    telefono varchar(45)  NOT NULL,
+    departamento varchar(2)  NOT NULL,
+    CONSTRAINT PERSONA_pk PRIMARY KEY (CI)
+);
+
+CREATE TABLE USUARIO (
+    CI int  NOT NULL,
+    usuario varchar(45)  NOT NULL,
+    password varchar(45)  NOT NULL,
+    CONSTRAINT USUARIO_pk PRIMARY KEY (CI)
+);
+
+
